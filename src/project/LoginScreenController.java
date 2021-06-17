db2f374a6833953dd524bd8209ad806ee3a17a32
Waleed Abdullah <68333922+Waleed-abdullah@users.noreply.github.com>
2021-06-17 13:24:52 +0500

    Add files via upload

--- src/project/LoginScreenController.java
+++ src/project/LoginScreenController.java
@@ -3,47 +3,49 @@ package project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginScreenController {
-
-    @FXML
-    private AnchorPane scenePane;
+public class LoginScreenController implements Initializable {
 
     Stage stage;
-
     @FXML
-    void closeApp(MouseEvent event) {
-            stage = (Stage)scenePane.getScene().getWindow();
-            stage.close();
-    }
-
+    private AnchorPane scenePane;
     @FXML
-    void minimizeApp(MouseEvent event) {
-        stage = (Stage)scenePane.getScene().getWindow();
-        stage.setIconified(true);
-
-    }
+    private ImageView ExitButton;
+    @FXML
+    private ImageView MinimizeButton;
 
     @FXML
     void login(ActionEvent event) throws IOException {
-            Parent root = FXMLLoader.load(getClass().getResource("MenuPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MenuPage.fxml"));
 
-            Scene menuScene = new Scene(root);
+        Scene menuScene = new Scene(root);
 
-            stage = (Stage) scenePane.getScene().getWindow();
-            stage.setScene(menuScene);
+        stage = (Stage) scenePane.getScene().getWindow();
+        stage.setScene(menuScene);
 
-            stage.show();
-
+        stage.show();
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ExitButton.setOnMouseClicked(event -> System.exit(0));
+
+        MinimizeButton.setOnMouseClicked(event -> {
+            stage = (Stage) scenePane.getScene().getWindow();
+            stage.setIconified(true);
+        });
+
+    }
 
 }
